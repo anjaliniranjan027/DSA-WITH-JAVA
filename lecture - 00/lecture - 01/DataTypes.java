fd3376735b2e654f991ac32967ca9f3b199cabc7
anjaliniranjan027 <142167388+anjaliniranjan027@users.noreply.github.com>
2024-08-31 12:19:03 +0530

    next

---
+++ lecture - 00/lecture - 01/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 100; // valid value
+        short s = 10000; // valid value
+        int i = 100000; // valid value
+        long l = 10000000000L; // Note the 'L' suffix
+        float f = 3.14f; // Note the 'f' suffix
+        double d = 3.14159; // No suffix needed
+        char c = 'A'; // Character literal
+        boolean flag = true; // Boolean literal
+
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + c);
+        System.out.println("boolean value: " + flag);
+    }
+}
