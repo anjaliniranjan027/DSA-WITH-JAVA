88fc5bc80181a8a6d2a6dcc9d7f858d468a29ca0
anjaliniranjan027 <142167388+anjaliniranjan027@users.noreply.github.com>
2024-09-01 17:05:31 +0530

    loops

--- lecture - 00/lecture - 01/Loops.java
+++ lecture - 00/lecture - 01/Loops.java
@@ -1,5 +1,24 @@
 public class Loops {
     public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.err.println(i);
+            i++ ;
+        } while(i<=10) ;
+
+        int j = 0;
+        while(j<=10) {
+            System.out.print(j);
+            j++ ;
+
+        }
+        for(int k=0; k<=10; k++) {
+            System.out.print(k);
+    }
+
+    
+
+        
         
     }
     
