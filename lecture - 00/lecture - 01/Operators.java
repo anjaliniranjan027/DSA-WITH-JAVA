70075d26563560585fd57ca8d4cad89f45f18996
anjaliniranjan027 <142167388+anjaliniranjan027@users.noreply.github.com>
2024-09-01 16:52:35 +0530

    back

--- lecture - 00/lecture - 01/Operators.java
+++ lecture - 00/lecture - 01/Operators.java
@@ -1,5 +1,22 @@
-package lecture - 01;
+// package lecture - 01;
 
 public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        // Arirhmatic operators [+,-,*,/,% ]
+        System.out.println("Addition of two numbers : " + (a+b));
+        System.out.println("Substraction of two numbers : " + (a-b));
+        System.out.println("Multiplication of two numbers : " + (a*b));
+        System.out.println("Division of two numbers : " + (a/b));
+        System.out.println("Modulus of two numbers : " + (a%b));
+
+        // Assignment operators
+        
+        
+    }
+
+    
+
     
 }
